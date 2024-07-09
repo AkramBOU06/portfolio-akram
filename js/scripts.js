c551287f30499a9e7f0c230d7cf85698af029e50
Akram Bouchoucha <akrambouchoucha@gmail.com>
2024-07-09 16:00:44 +0200

    Features: mise en place d'un défilement fluide

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav a");
+
+    for (const link of links) {
+        link.addEventListener("click", smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    }
+});
